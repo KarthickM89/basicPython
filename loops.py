690f4f90bbe64835030c29cddbd658e399f96823
Karthick <karthickmanimaran@hotmail.com>
2021-07-03 12:03:48 +0530

    added loops file

---
+++ loops.py
@@ -0,0 +1,15 @@
+#for i in [0, 1, 2, 3, 4, 5]:
+# print(i)
+
+#for i in range(6):
+#  print(i)
+
+# Defining names
+names = ["karthick", "kaviya", "kavinilaa"]
+name1 = "kannan"
+
+# Looping and Printing
+for name in names:
+  print(name)
+for n in name1:
+  print(n)
