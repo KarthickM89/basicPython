1aafab4802e0f2902fd52f44fc32c76eeb0a0eeb
Karthick <karthickmanimaran@hotmail.com>
2021-07-03 12:23:20 +0530

    added dict

---
+++ dictinories.py
@@ -0,0 +1,9 @@
+# Defining houses
+houses = {"Karthick": "Palani", "Kaviya": "Thumba"}
+
+# Adding extra key values to the list
+houses["Kannan"]= "Palani"
+houses["Kavinilaa"]= "Thumba"
+
+# Printing specific key 
+print(houses["Kavinilaa"])
