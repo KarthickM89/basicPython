151b5db2fa304698407d6c89f2e4c48176bfc49a
Karthick <karthickmanimaran@hotmail.com>
2021-07-03 11:45:45 +0530

    add set

---
+++ set.py
@@ -0,0 +1,16 @@
+# Defining a set
+s = set()
+
+# Adding numbers to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+# Removing numbers to the set
+s.remove(2)
+
+# Printing it
+print(s)
+print(f"There are {len(s)} numbers of elements on the set")
